5e3ea63067e0c2d280e9af563113413127b74f05
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:54:05 +0300

    add team_leaderC.cs

---
+++ FleetManager.Shared/Models/sync/team_leaderC.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class team_leaderC :serverC
+    {
+        
+        public int team_leader_id { get; set; }
+        public string team_leader_name { get; set; }
+        public int user_id { get; set; }
+        public string phone_no { get; set; }
+
+        //
+    }
+}
