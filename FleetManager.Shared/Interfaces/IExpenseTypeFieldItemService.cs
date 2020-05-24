66dc9ce2819b1324e85ada10546b3fbfd0eafaaf
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:48:54 +0300

    add IExpenseTypeFieldItemService.cs

---
+++ FleetManager.Shared/Interfaces/IExpenseTypeFieldItemService.cs
@@ -0,0 +1,16 @@
+﻿using FleetManager.Shared.Models;
+using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+   public interface IExpenseTypeFieldItemService : IServiceCoreSingle<Models.vh_expense_type_field_d_itemC>
+    {
+        Task<vh_expense_type_field_d_itemC> AddExpenseTypeFieldItem(dto.dto_expense_type_field_item_newC _dto);
+        Task<vh_expense_type_field_d_itemC> UpdateExpenseTypeFieldItem(dto.dto_expense_type_field_item_updateC _dto);
+    }
+}
