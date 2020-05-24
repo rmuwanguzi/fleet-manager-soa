2beda57641844011d1710ee4c5c743bf6a93e0a9
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:42:16 +0300

    add CheckForSoftwareUpdatesService.cs

---
+++ FleetManager.Services/Services/common/CheckForSoftwareUpdatesService.cs
@@ -0,0 +1,19 @@
+﻿using MutticoFleet.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MutticoFleet.Shared.dto;
+using MutticoFleet.Service;
+using Rebex.Net;
+using System.Net;
+
+namespace MutticoFleet.Services
+{
+   public class CheckForSoftwareUpdatesService : ICheckForSoftwareUpdatesService
+    {
+      
+	  
+    }
+}
