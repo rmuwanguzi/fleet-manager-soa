d14f2c9c293f242668cf7351f6333ad5ca1ffdb6
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:52:14 +0300

    add vh_expense_type_field_d_itemC.cs

---
+++ FleetManager.Shared/Models/sync/vh_expense_type_field_d_itemC.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class vh_expense_type_field_d_itemC : serverC
+    {
+        
+        public int et_field_item_id { get; set; }
+        public string et_field_item_name { get; set; }
+        public int exp_type_id { get; set; }
+        public int et_field_id { get; set; }
+        public string et_field_name { get; set; }
+    }
+}
