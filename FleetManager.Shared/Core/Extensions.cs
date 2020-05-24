f81cfa4078d260405ccbb2a1afa33aceffdbbe29
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:43:49 +0300

    add Extensions.cs

---
+++ FleetManager.Shared/Core/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Core
+{
+    public static class Extensions
+    {
+      
+        public static long ToUnixTimestamp(this DateTime value)
+        {
+            return (long)Math.Truncate((value.ToUniversalTime().Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+        }
+    }
+}
