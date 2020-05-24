b57b06ae6a74e13a953c6a9c951228f0421b03c7
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:49:59 +0300

    add IVehicleCatService.cs

---
+++ FleetManager.Shared/Interfaces/IVehicleCatService.cs
@@ -0,0 +1,17 @@
+﻿using FleetManager.Shared.Models;
+using FleetManager.Shared.dto;
+using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+    public interface IVehicleCatService : IServiceCoreSingle<vh_categoryC>
+    {
+        Task<vh_categoryC> AddVehicleCategory(dto_vehicle_category_newC dto);
+        Task<vh_categoryC> UpdateVehicleCategory(dto_vehicle_category_updateC dto);
+    }
+}
