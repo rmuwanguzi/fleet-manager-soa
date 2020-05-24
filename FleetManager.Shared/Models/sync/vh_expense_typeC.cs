eaa79fc0b9a14fffd93a9323da0bc53765246814
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:52:01 +0300

    add vh_expense_typeC.cs

---
+++ FleetManager.Shared/Models/sync/vh_expense_typeC.cs
@@ -0,0 +1,27 @@
+﻿using FleetManager.Shared;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+   public class vh_expense_typeC : serverC
+    {
+        
+        public int exp_type_id { get; set; }
+        public string exp_type_name { get; set; }
+        public int exp_cat_id { get; set; }
+        public int field_count { get; set; }
+        public em.vehicle_expense_categoryS exp_cat
+        {
+            get
+            {
+               return (em.vehicle_expense_categoryS)exp_cat_id;
+            }
+        }
+    }
+}
