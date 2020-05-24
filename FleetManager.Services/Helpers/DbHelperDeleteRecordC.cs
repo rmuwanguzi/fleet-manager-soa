10b28e3201aba122e7ff2ba6e574adc7f5b55812
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:38:33 +0300

    add DbHelperDeleteRecordC.cs

---
+++ FleetManager.Services/Helpers/DbHelperDeleteRecordC.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace MutticoFleet.Service
+{
+    internal class DbHelperDeleteRecordC
+    {
+        public string table_name { get; set; }
+        public string pk_col_name { get; set; }
+        public int pk_id { get; set; }
+    }
+}
