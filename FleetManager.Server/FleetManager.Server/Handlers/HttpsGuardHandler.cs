4f959728fa84ef654f9370ebc76b8f72272cd0d6
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:34:20 +0300

    add HttpsGuardHandler.cs

---
+++ FleetManager.Server/FleetManager.Server/Handlers/HttpsGuardHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SmartITAzure.Handlers
+{
+    public class HttpsGuardHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.RequestUri.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
+            {
+                HttpResponseMessage reply = request.CreateErrorResponse(HttpStatusCode.BadRequest, "HTTPS is required for security reasons.");
+                return Task.FromResult(reply);
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
