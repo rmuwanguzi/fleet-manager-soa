e505478953226eebddc48545e86810661ac013d2
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:52:59 +0300

    add fuel_stationC.cs

---
+++ FleetManager.Shared/Models/sync/fuel_stationC.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class fuel_stationC : serverC
+    {
+        
+        public int fuel_station_id { get; set; }
+        public string fuel_station_name { get; set; }
+        public int cr_account_id { get; set; }
+        public int cr_balance_amount { get; set; }
+        public string contact_person { get; set; }
+        public string phone_no { get; set; }
+    }
+}
