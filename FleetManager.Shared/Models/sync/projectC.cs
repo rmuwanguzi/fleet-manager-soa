b726155ccb776b5922d715c1822327c4d27fe20f
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:53:06 +0300

    add projectC.cs

---
+++ FleetManager.Shared/Models/sync/projectC.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class projectC : serverC
+    {
+        public string project_name { get; set;}
+        
+        public int project_id { get; set;}
+        public int no_of_fuel_stations { get; set; }
+
+    }
+}
