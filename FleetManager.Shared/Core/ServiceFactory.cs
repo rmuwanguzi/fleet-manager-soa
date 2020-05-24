73ae1f3894bf1856a04a14403649097b0feb66dc
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:44:07 +0300

    add ServiceFactory.cs

---
+++ FleetManager.Shared/Core/ServiceFactory.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using FleetManager.Shared.Interfaces;
+
+namespace FleetManager.Shared.Core
+{
+    public class ServiceFactory : IServiceFactory
+    {
+       public T GetService<T>() where T : class, IService
+        {
+          return ObjectBase._container == null ? default(T) : ObjectBase._container.Resolve<T>();
+        }
+
+    }
+}
