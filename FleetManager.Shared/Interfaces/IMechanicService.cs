1a291d8560f5d639215f9c812f6b0de1c516728d
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:49:54 +0300

    add IMechanicService.cs

---
+++ FleetManager.Shared/Interfaces/IMechanicService.cs
@@ -0,0 +1,14 @@
+﻿using FleetManager.Shared.dto;
+using FleetManager.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+    public interface IMechanicService : IServiceCore<mechanicC, dto_mechanic_newC, dto_mechanic_updateC>
+    {
+    }
+}
