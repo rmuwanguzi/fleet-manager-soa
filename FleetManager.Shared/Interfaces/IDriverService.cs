6b07234e80719145d705fe09fd57636d891ea94b
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:49:49 +0300

    add IDriverService.cs

---
+++ FleetManager.Shared/Interfaces/IDriverService.cs
@@ -0,0 +1,15 @@
+﻿using FleetManager.Shared.Models;
+using FleetManager.Shared.dto;
+using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+    public interface IDriverService : IServiceCore<vh_driverC, dto_driver_newC, dto_driver_updateC>
+    {
+    }
+}
