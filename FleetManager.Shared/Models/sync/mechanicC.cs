daf864cd92c2977c2e441386678e8a92c6553763
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:53:58 +0300

    add mechanicC.cs

---
+++ FleetManager.Shared/Models/sync/mechanicC.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class mechanicC : serverC
+    {
+        public string mechanic_name { get; set;}
+        public string phone_no { get; set;}
+        
+        public int mechanic_id { get; set;}
+        public int cr_account_id { get; set; }
+        public int cr_balance_amount { get; set; }
+    }
+}
