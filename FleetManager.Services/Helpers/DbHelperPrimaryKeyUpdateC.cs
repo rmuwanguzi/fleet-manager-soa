5e45d50c428d6b4b52496b33359b044fa59a9d98
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:38:59 +0300

    add DbHelperPrimaryKeyUpdateC.cs

---
+++ FleetManager.Services/Helpers/DbHelperPrimaryKeyUpdateC.cs
@@ -0,0 +1,12 @@
+﻿namespace MutticoFleet.Service
+{
+    internal class DbHelperPrimarykeyUpdateC
+    {
+
+        public string col_to_update { get; set; }
+        public string new_col_value { get; set; }
+        public string table_name { get; set; }
+        public string pk_col_name { get; set; }
+        public int pk_id { get; set; }
+    }
+}
