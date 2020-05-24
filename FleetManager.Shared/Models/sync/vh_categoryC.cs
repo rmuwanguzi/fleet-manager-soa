f892494cc83dc4ab61b59ecea4d670d20bb7d519
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:53:23 +0300

    add vh_categoryC.cs

---
+++ FleetManager.Shared/Models/sync/vh_categoryC.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+   
+   public class vh_categoryC : serverC
+    {
+        
+        public int vehicle_cat_id { get; set; }
+        public string vehicle_cat_name { get; set; }
+    }
+}
