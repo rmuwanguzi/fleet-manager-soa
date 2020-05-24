2c9d193f11bc6eb5410a371bb80e73c2846c07bf
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:38:40 +0300

    add DbHelperUpdateDeleteIdC.cs

---
+++ FleetManager.Services/Helpers/DbHelperUpdateDeleteIdC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutticoFleet.Service
+{
+  internal class DbHelperUpdateDeleteIdC
+    {
+        public string col_to_update { get; set; }
+        public int new_col_value { get; set; }
+        public string table_name { get; set; }
+        public string pk_col_name { get; set; }
+        public int pk_id { get; set; }
+    }
+}
