a28911fdf22bb67d8b3eeaf6402313fa5ec09c11
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:30:26 +0300

    add Extensions/

---
+++ FleetManager.Server/FleetManager.Server/Extensions/ExtensionsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MutticoFleet.Server
+{
+    public static class ExtensionsController
+    {
+        public static string ToErrorMessage(this System.Web.Http.ModelBinding.ModelStateDictionary modelstate)
+        {
+            try
+            {
+                var  messages = string.Join("; ", modelstate.Values
+                                         .SelectMany(x => x.Errors)
+                                         .Select(x => x.ErrorMessage));
+                    return messages;
+                
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
