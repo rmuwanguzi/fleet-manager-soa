57941630930a9738c683cdd2c376c21c22d2b6de
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:30:12 +0300

    add DbContext/

---
+++ FleetManager.Server/FleetManager.Server/DbContext/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using FleetManager.DbBase;
+using MutticoFleet.Server;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace MutticoFleet.Server
+{
+    public class DbContext : BaseContext
+    {
+        public DbContext() : base(new SqlConnection(fn.CONN_STRING), true, "fleet_dev")
+        {
+            
+        }
+    }
+}
