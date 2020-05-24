287262d1f991ec989967bfdf876b43fd23f01a98
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:50:21 +0300

    add IExpenseTypeService.cs

---
+++ FleetManager.Shared/Interfaces/IExpenseTypeService.cs
@@ -0,0 +1,16 @@
+﻿using FleetManager.Shared.Models;
+using FleetManager.Shared.dto;
+using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+   public interface IExpenseTypeService : IServiceCore<vh_expense_typeC, dto_expense_type_newC, dto_expense_type_updateC>
+    {
+             
+    }
+}
