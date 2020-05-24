d49feb407ed161f07d836ff61c3ed5bd3c8e74cb
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:33:46 +0300

    add MessageDialog.cs

---
+++ FleetManager.Server/FleetManager.Server/Platform_Specific/MessageDialog.cs
@@ -0,0 +1,23 @@
+﻿using FleetManager.Shared.Interfaces;
+
+
+namespace MutticoFleet.Server.Platform_Specific
+{
+    public class MessageDialog : IMessageDialog
+    { 
+       
+        public void ErrorMessage(string message, string title = null)
+        {
+            return;
+        }
+        public object tag { get; set; }
+        public void ErrorMessage(string key, string message, string controller_key = null)
+        {
+            if (!string.IsNullOrWhiteSpace(controller_key))
+            {
+                var modelState = datam.DATA_CONTROLLER_MODEL_STATE[controller_key];
+                modelState.AddModelError(controller_key, message);
+            }
+         }
+    }
+}
