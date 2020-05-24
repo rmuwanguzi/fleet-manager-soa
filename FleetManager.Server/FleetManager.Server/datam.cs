8577937d0cb9cf37d73d5c656a5786e8bad30546
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:31:33 +0300

    add datam.cs

---
+++ FleetManager.Server/FleetManager.Server/datam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.ModelBinding;
+
+namespace MutticoFleet.Server
+{
+    public class datam
+    {
+        public static SortedList<string, ModelStateDictionary> DATA_CONTROLLER_MODEL_STATE { get; set; }
+    }
+}
