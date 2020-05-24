a79892caa7a6e5029db8b3255b4fb1ec31b29057
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:34:36 +0300

    add PreflightRequestsHandler.cs

---
+++ FleetManager.Server/FleetManager.Server/Handlers/PreflightRequestsHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MutticoFleet.Server.Handlers
+{
+    public class PreflightRequestsHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.Headers.Contains("Origin") && request.Method.Method.Equals("OPTIONS"))
+            {
+                var response = new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
+                // Define and add values to variables: origins, headers, methods (can be global)               
+                response.Headers.Add("Access-Control-Allow-Origin", "*");
+                response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Accept,XAuth-Token,Content-Length,X-Requested-With,Origin,Authorization");
+                response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS");
+                var tsc = new TaskCompletionSource<HttpResponseMessage>();
+                tsc.SetResult(response);
+                return tsc.Task;
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+
+    }
+}
