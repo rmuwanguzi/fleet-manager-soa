09c67e81b70958eda40c3f58fa9c466b60e9bb81
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:51:38 +0300

    add assign_project_fuel_stationC.cs

---
+++ FleetManager.Shared/Models/sync/assign_project_fuel_stationC.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class assign_project_fuel_stationC : serverC
+    {
+        
+        public int un_id { get; set; }
+        public int project_id { get; set; }
+        public int fuel_station_id { get; set; }
+    }
+}
