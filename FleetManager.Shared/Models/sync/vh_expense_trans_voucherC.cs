ce0af31c3ffdb1a171513d0b3709d5d2af07aa4e
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:52:22 +0300

    add vh_expense_trans_voucherC.cs

---
+++ FleetManager.Shared/Models/sync/vh_expense_trans_voucherC.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class vh_expense_trans_voucherC : serverC
+    {
+        
+        public int voucher_id { get; set; }
+        public string voucher_no { get; set; }
+        public int vehicle_id { get; set; }
+        public string vehicle_plate_no { get; set; }
+        public int driver_id { get; set; }
+        public string driver_name { get; set; }
+        public int voucher_total_amount { get; set; }
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public DateTime? expense_date { get; set; }
+        public int expense_fs_id { get; set; }
+        public int m_partition_id { get; set; }
+
+
+
+    }
+}
