8d91b5c68124b25c8dd7aa41e31a8fb9bdca6583
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:49:40 +0300

    add IVehicleOwnerService.cs

---
+++ FleetManager.Shared/Interfaces/IVehicleOwnerService.cs
@@ -0,0 +1,14 @@
+﻿using FleetManager.Shared.dto;
+using FleetManager.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+    public interface IVehicleOwnerService : IServiceCore<vehicle_ownerC, dto_vehicle_owner_newC, dto_vehicle_owner_updateC>
+    {
+    }
+}
