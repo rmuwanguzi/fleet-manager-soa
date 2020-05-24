524b5e728a72d93430862d10452e7b4ce10ea1b9
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:49:00 +0300

    add ITeamLeaderService.cs

---
+++ FleetManager.Shared/Interfaces/ITeamLeaderService.cs
@@ -0,0 +1,14 @@
+﻿using FleetManager.Shared.dto;
+using FleetManager.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+    public interface ITeamLeaderService : IServiceCore<team_leaderC, dto_team_leader_newC, dto_team_leader_updateC>
+    {
+    }
+}
