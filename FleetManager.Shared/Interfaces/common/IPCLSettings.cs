b05db31ddea6888e743cac0c126bf7f819adfff5
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:51:09 +0300

    add IPCLSettings.cs

---
+++ FleetManager.Shared/Interfaces/common/IPCLSettings.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace FleetManager.Shared.Interfaces
+{
+   public interface IPCLSettings
+    {
+        string BaseUrl { get; set; }
+        string errorLogRootFolderPath { get; set; }
+        bool isDevEnviroment { get; set; }
+    }
+}
