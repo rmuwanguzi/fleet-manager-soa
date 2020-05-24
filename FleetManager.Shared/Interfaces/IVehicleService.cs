bd1e61d0a4099b4eb6999df4244e5bc59867bb51
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:49:07 +0300

    add IVehicleService.cs

---
+++ FleetManager.Shared/Interfaces/IVehicleService.cs
@@ -0,0 +1,17 @@
+﻿using FleetManager.Shared.Models;
+using FleetManager.Shared.dto;
+using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+   public  interface IVehicleService :IServiceCore<vehicleC, dto_vehicle_newC, dto_vehicle_updateC>, IService
+    {
+      
+       
+    }
+}
