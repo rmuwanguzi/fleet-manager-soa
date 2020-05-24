985c5699c2fc4c49e46500d69a92cc0c27ed2f82
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:52:31 +0300

    add vh_expense_trans_fieldC.cs

---
+++ FleetManager.Shared/Models/sync/vh_expense_trans_fieldC.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class vh_expense_trans_fieldC : serverC
+    {
+        public string field_name { get; set; }
+        public string field_value { get; set; }
+        public int expense_id { get; set; }
+        public int exp_type_id { get; set; }
+        public int et_field_id { get; set; }//mileage
+        public int et_field_item_id { get; set; }
+        
+        public int un_id { get; set; }
+        public int voucher_id { get; set; }
+    }
+}
