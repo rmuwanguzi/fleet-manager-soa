c3d9212f761bbe36b099fa0f9e05d79afb48137b
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:33:38 +0300

    add LoggedInUserService.cs

---
+++ FleetManager.Server/FleetManager.Server/Platform_Specific/LoggedInUserService.cs
@@ -0,0 +1,24 @@
+﻿using FleetManager.Shared.dto;
+using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MutticoFleet.Server.Platform_Specific
+{
+    public class LoggedInUserService : ILoggedInUserService
+    {
+       public string controller_key { get; set; }
+        private dto_logged_user _l_user { get; set; }
+        public dto_logged_user GetLoggedInUser()
+        {
+            return _l_user;
+        }
+        public LoggedInUserService()
+        {
+            _l_user = new dto_logged_user();
+        }
+    }
+}
