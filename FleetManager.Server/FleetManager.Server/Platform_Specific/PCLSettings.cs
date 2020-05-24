1d1b9e9cade2d4763a9944d35ae7d96da6cb5516
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:33:55 +0300

    add PCLSettings.cs

---
+++ FleetManager.Server/FleetManager.Server/Platform_Specific/PCLSettings.cs
@@ -0,0 +1,17 @@
+﻿
+using FleetManager.Shared.Interfaces;
+
+namespace MutticoFleet.Server.Platform_Specific
+{
+    public class PCLSettings : IPCLSettings
+    {
+        public string BaseUrl
+        {
+            get; set;
+        }
+        public string DB_PATH { get; set; }
+        public string controller_key { get; set; }
+        public string errorLogRootFolderPath { get; set; }
+        public bool isDevEnviroment { get; set; }
+    }
+}
