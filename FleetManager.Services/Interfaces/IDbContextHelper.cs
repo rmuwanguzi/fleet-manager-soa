f88cf5765042225be7a48f1f5a6f5d922bd994eb
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:39:33 +0300

    add IDbContextHelper.cs

---
+++ FleetManager.Services/Interfaces/IDbContextHelper.cs
@@ -0,0 +1,16 @@
+﻿using FleetManager.DbBase;
+using FleetManager.Shared;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+
+namespace MutticoFleet.Service.Interfaces
+{
+   public interface IDbContextHelper
+    {
+        BaseContext GetContext();
+        DbParameter GetDbParameters(string parameterName, object value);
+        bool? IsSaveDuplicateRecordError(DbUpdateException ex);
+        bool? IsUpdateDuplicateRecordError(DbException ex);
+        DbConnection GetDBConnection();
+    }
+}
