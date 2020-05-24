89f9ccfbe1ecdb6ea4037f722cd600a786e9e0d6
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:43:56 +0300

    add IServiceFactory.cs

---
+++ FleetManager.Shared/Core/IServiceFactory.cs
@@ -0,0 +1,17 @@
+﻿using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FleetManager.Shared.Core
+{
+    public interface IServiceFactory
+    {
+       
+       T GetService<T>() where T : class, IService;
+    }
+   
+}
