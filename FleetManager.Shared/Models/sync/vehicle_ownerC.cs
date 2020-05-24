4e810c65fafd3e3497df19bc8150eb967b4b5004
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:51:46 +0300

    add vehicle_ownerC.cs

---
+++ FleetManager.Shared/Models/sync/vehicle_ownerC.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Models
+{
+    
+    public class vehicle_ownerC : serverC
+    {
+        public string vh_owner_name { get; set; }
+        
+        public int vh_owner_id { get; set; }
+        public string vh_owner_phone_no { get; set;}
+        public int cr_account_id { get; set; }
+        public int cr_balance_amount { get; set; }
+    }
+}
