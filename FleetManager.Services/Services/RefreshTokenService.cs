7d59aa35d3e2d8ddfe9d8b61e9fed94b76e9acef
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:41:54 +0300

    add RefreshTokenService.cs

---
+++ FleetManager.Services/Services/RefreshTokenService.cs
@@ -0,0 +1,21 @@
+﻿using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FleetManager.Shared.dto;
+using MutticoFleet.Service;
+
+namespace MutticoFleet.Services
+{
+    public class RefreshTokenService : IRefreshTokenService
+    {
+        public string controller_key { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task<dto_refresh_tokenC> GetRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
