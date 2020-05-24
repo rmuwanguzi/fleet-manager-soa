8ba42773239644b940c612309a5a41447a7879af
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:38:25 +0300

    add DataExtensions.cs

---
+++ FleetManager.Services/Helpers/DataExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MutticoFleet.Service
+{
+    internal static class DataExtensions
+    {
+        public static IEnumerable<T> ToFullyLoaded<T>(this IQueryable<T> query)
+        {
+            return query.ToArray().ToList();
+        }
+        public static IEnumerable<T> ToFullyLoaded<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.ToList();
+        }
+    }
+}
