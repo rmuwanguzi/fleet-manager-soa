aa5a440cce96afbe2004ae74befe347c5e537012
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:29:16 +0300

    add App_Start/

---
+++ FleetManager.Server/FleetManager.Server/App_Start/WebApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Dispatcher;
+
+namespace MutticoFleet.Server
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.MessageHandlers.Add(new Handlers.PreflightRequestsHandler());
+            config.MessageHandlers.Add(new Handlers.XAccessTokenHandler());
+       
+            config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+
+             );
+           // var cors = new EnableCorsAttribute("www.example.com", "*", "*");
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+         
+
+            if (datam.DATA_CONTROLLER_MODEL_STATE == null)
+            {
+                datam.DATA_CONTROLLER_MODEL_STATE = new SortedList<string, System.Web.Http.ModelBinding.ModelStateDictionary>();
+            }
+            fn.Injection();
+        }
+    }
+}
