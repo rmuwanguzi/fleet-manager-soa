e13c16e693e44eddf6b869ba7f7854c4f2efc00b
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:49:29 +0300

    add IFuelStationService.cs

---
+++ FleetManager.Shared/Interfaces/IFuelStationService.cs
@@ -0,0 +1,16 @@
+﻿using FleetManager.Shared.Models;
+using FleetManager.Shared.dto;
+using FleetManager.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+    public interface IFuelStationService : IServiceCore<fuel_stationC, dto_fuel_station_newC, dto_fuel_station_updateC>
+    {
+
+    }
+}
