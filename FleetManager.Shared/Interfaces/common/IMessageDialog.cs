ca53c8b8dd40b305810b97f4c8810e7a7cd5ff9e
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-24 21:50:57 +0300

    add IMessageDialog.cs

---
+++ FleetManager.Shared/Interfaces/common/IMessageDialog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManager.Shared.Interfaces
+{
+    public interface IMessageDialog
+    {
+    
+        void ErrorMessage(string message, string title = null);
+        void ErrorMessage(string key, string message, string controller_key=null);
+         
+    }
+}
